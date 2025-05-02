dc1cdadcb25bb90d5a0acc2331739f63136cba82
shivam shukla <shivamshukla8112005@gmail.com>
2025-05-02 18:31:00 +0530

    user registration and login routes has been created and tested using thunderclient successfully

---
+++ Skill-Swap Backend/routes/authRoutes.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+
+router.post('/register', async(req, res) => {
+    const {name, email, password, skills} = req.body;
+    try{
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const salt= await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser=new User({name, email, password: hashedPassword, skills});
+        await newUser.save();
+        res.status(201).json({message: 'User registered successfully'});
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+router.post('/login', async(req, res)=>{
+    const {email, password}=req.body;
+    const userExist=await User.findOne({email});
+    if(!userExist){
+       return res.status(404).json({message:' user not found'});
+    }
+    const isMatch = await bcrypt.compare(password, userExist.password);
+    if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: userExist._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+      });
+    res.status(200).json( {token, message:'Login successful', user: { id: userExist._id, name: userExist.name, email: userExist.email }});
+})
+export default router;
--- Skill-Swap Backend/server.js
+++ Skill-Swap Backend/server.js
@@ -2,18 +2,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 const app = express();
-app.use(cors());
+//Middleware
 app.use(express.json());
+app.use(cors());
 
-// Basic test route
-app.get('/', (req, res) => {
+// routes
+app.use('/api/users',userRoutes);
+app.use('/api/auth',authRoutes);
+mongoose.connect(process.env.MONGO_URI).then(() => {console.log('MongoDB connected')}).catch((error) => {console.log(`MongoDB connection error: ${error}`)});
+
+// mongodb://localhost:27017/skill-swap
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch((error) => console.error(`MongoDB connection error: ${error}`));
+
+  app.get('/', (req, res) => {
   res.send('API is running............');
 });
-
-// MongoDB connect (we'll do it after .env setup)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
