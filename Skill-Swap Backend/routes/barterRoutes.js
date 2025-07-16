734d2e91803f87b3067d4bbfd95c1b5faacf2913
shivam shukla <shivamshukla8112005@gmail.com>
2025-07-16 09:42:35 +0530

    written getBarter api also write frontend to fetch skillposts and post skill post

--- Skill-Swap Backend/routes/barterRoutes.js
+++ Skill-Swap Backend/routes/barterRoutes.js
@@ -6,6 +6,23 @@ import SkillPost from '../models/skillPost.js';
 
 const router = express.Router();
 
+
+
+router.get('/getBarter', verifyToken, async (req, res) => {
+    try {
+        const barter = await SkillPost.find()
+        if (barter) {
+            return res.status(200).json({status: 200, barter: barter})
+        }
+        else {
+            return res.status(404).json({status: 404, barter: []})
+        }
+    } catch (error) {
+        console.log(error)        
+    }
+})
+
+
 router.post('/request', verifyToken, async (req, res) => {
     const { receiverId, senderSkillPostId, receiverSkillPostId, newSkillPost } = req.body;
     try {
