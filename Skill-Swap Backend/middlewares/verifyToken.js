85c2266910bf63408dedfe5c6b4e0bdf1a158576
shivam shukla <shivamshukla8112005@gmail.com>
2025-07-21 23:46:52 +0530

    made code more appropriate and cleaner

--- Skill-Swap Backend/middlewares/verifyToken.js
+++ Skill-Swap Backend/middlewares/verifyToken.js
@@ -1,35 +1,4 @@
-// import jwt from 'jsonwebtoken';
-// import User from '../models/userModel.js';
-
-// const verifyToken = async (req, res, next) => {
-//     try{
-//         // console.log(req.user);
-//         const authHeader = req.headers.authorization;
-//         if(!authHeader || !authHeader.startsWith("Bearer ")){
-//             return res.status(401).json({message:'Unauthorized'});
-//         }
-//         const token = authHeader.split(" ")[1];
-//         // console.log(token);
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         const user=await User.findById(decoded.id);
-//         if(!user){
-//             return res.status(404).json({message:'User not found'});
-//         }
-//         req.user = user;
-//         next();
-//     }catch(error){
-//         console.error(error);
-//         // console.log('helooooooooooooooooooooooooooooooooooo');
-        
-//         return res.status(401).json({message:'Unauthorized'});
-//     }
-// }
-// export default verifyToken;
-
-
-
 import jwt from 'jsonwebtoken'
-// import { ApiError } from './ApiError.js'
 
  const verifyToken = (req, res, next) => {
 
