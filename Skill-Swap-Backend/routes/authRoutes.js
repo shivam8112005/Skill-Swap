de35250fae7a4f7247c5ba384eac96f445040626
shivam shukla <shivamshukla8112005@gmail.com>
2025-08-06 01:02:36 +0530

    change cookie option in login signup

--- Skill-Swap-Backend/routes/authRoutes.js
+++ Skill-Swap-Backend/routes/authRoutes.js
@@ -111,7 +111,7 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ message: 'User already exists' });
         }
          const cookieOption = {
-                    // httpOnly: true,
+                    httpOnly: true,
                     secure: true,
                     sameSite: "None",
                     maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -149,7 +149,7 @@ router.post('/login', async (req, res) => {
         expiresIn: "2w"
     });
      const cookieOption = {
-                    // httpOnly: true,
+                    httpOnly: true,
                     secure: true,
                     sameSite: "None",
                     maxAge: 7 * 24 * 60 * 60 * 1000,
