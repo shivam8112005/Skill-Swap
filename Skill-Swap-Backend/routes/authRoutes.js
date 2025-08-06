7d2e73ed9b99269469a7f603dc8e8bc35c0c6e43
shivam shukla <shivamshukla8112005@gmail.com>
2025-08-06 15:26:11 +0530

    edited cookie option of login route

--- Skill-Swap-Backend/routes/authRoutes.js
+++ Skill-Swap-Backend/routes/authRoutes.js
@@ -150,8 +150,8 @@ router.post('/login', async (req, res) => {
     });
      const cookieOption = {
                     httpOnly: true,
-                    secure: false,
-                    sameSite: "LAX",
+                    secure: true,
+                    sameSite: "None",
                     maxAge: 7 * 24 * 60 * 60 * 1000,
                     path: "/",
                 };
